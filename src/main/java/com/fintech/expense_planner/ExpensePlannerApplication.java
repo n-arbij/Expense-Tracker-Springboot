4e6b14baec00699a2016e425e5db5a8781ec5ffb
n-arbij <omollojibran@gmail.com>
2026-06-02 08:47:07 +0300

    Initial Commit

---
+++ src/main/java/com/fintech/expense_planner/ExpensePlannerApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.expense_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensePlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fintech/expense_planner/ExpensePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.expense_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
